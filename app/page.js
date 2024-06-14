3f324af282c9a75d01448f20c66c3dc66c7e88ff
Aravind R <aravind.r@applab.qa>
2024-06-14 18:56:08 +0300

    Initial commit

--- app/page.js
+++ app/page.js
@@ -1,113 +1,158 @@
-import Image from "next/image";
+"use client";
+
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const initialState = [
+    ["", "", ""],
+    ["", "", ""],
+    ["", "", ""],
+  ];
+  const initialMoves = {
+    X: {
+      1: {},
+      2: {},
+      3: {},
+    },
+    O: {
+      1: {},
+      2: {},
+      3: {},
+    },
+  };
+
+  const [ticTacToe, setTicTacToe] = useState(initialState);
+  const [currentPlayer, setCurrentPlayer] = useState("X");
+  const [matchWon, setMatchWon] = useState(false);
+  const [previousMoves, setPreviousMoves] = useState(initialMoves);
+
+  useEffect(() => {
+    let pointTable = {};
+    for (let i = 0; i < ticTacToe.length; i++) {
+      for (let j = 0; j < ticTacToe[i].length; j++) {
+        if (!ticTacToe[i][j]) continue;
+        if (!pointTable[`row${i}`]) {
+          pointTable[`row${i}`] = {
+            prevEl: ticTacToe[i][j],
+            points: 1,
+          };
+        } else if (pointTable[`row${i}`].prevEl === ticTacToe[i][j]) {
+          pointTable[`row${i}`].points++;
+        }
+        if (!pointTable[`col${j}`]) {
+          pointTable[`col${j}`] = {
+            prevEl: ticTacToe[i][j],
+            points: 1,
+          };
+        } else if (pointTable[`col${j}`].prevEl === ticTacToe[i][j]) {
+          pointTable[`col${j}`].points++;
+        }
+        if (i === j) {
+          if (!pointTable[`dia1`]) {
+            pointTable[`dia1`] = {
+              prevEl: ticTacToe[i][j],
+              points: 1,
+            };
+          } else if (pointTable[`dia1`].prevEl === ticTacToe[i][j]) {
+            pointTable[`dia1`].points++;
+          }
+        }
+        if (ticTacToe[i].length - 1 - i === j) {
+          if (!pointTable[`dia2`]) {
+            pointTable[`dia2`] = {
+              prevEl: ticTacToe[i][j],
+              points: 1,
+            };
+          } else if (pointTable[`dia2`].prevEl === ticTacToe[i][j]) {
+            pointTable[`dia2`].points++;
+          }
+        }
+      }
+    }
+    Object.keys(pointTable).forEach((item) => {
+      if (pointTable[item].points === 3) setMatchWon(true);
+    });
+  }, [ticTacToe]);
+
+  const handleCellClick = (rowIndex, colIndex) => {
+    if (ticTacToe[rowIndex][colIndex]) {
+      return;
+    }
+
+    setTicTacToe((prevState) => {
+      prevState[rowIndex][colIndex] = currentPlayer;
+      if (previousMoves[currentPlayer]["3"].rowIndex !== undefined) {
+        prevState[previousMoves[currentPlayer]["3"].rowIndex][
+          previousMoves[currentPlayer]["3"].colIndex
+        ] = "";
+      }
+      return [...prevState];
+    });
+    setPreviousMoves((prevState) => {
+      prevState[currentPlayer]["3"] = prevState[currentPlayer]["2"];
+      prevState[currentPlayer]["2"] = prevState[currentPlayer]["1"];
+      prevState[currentPlayer]["1"] = {
+        rowIndex,
+        colIndex,
+      };
+      return { ...prevState };
+    });
+    setCurrentPlayer((prevState) => (prevState === "X" ? "O" : "X"));
+  };
+
+  const resetGame = () => {
+    setTicTacToe(initialState);
+    setMatchWon(false);
+    setCurrentPlayer("X");
+    setPreviousMoves(initialMoves);
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="flex flex-col justify-center w-screen h-screen bg-gray-600 text-white text-center">
+      <div className="fixed top-0 left-0 right-0 h-24 flex justify-center items-center">
+        <h1 className="font-bold text-3xl font-mono">Super TicTacToe</h1>
+      </div>
+      {matchWon && (
+        <div className="fixed top-0 bottom-0 left-0 right-0 bg-black bg-opacity-50 flex justify-center items-center">
+          <div className="backdrop-blur-sm p-20 rounded-lg w-full h-full flex justify-between items-center flex-col">
+            <p className="font-bold text-lg">
+              {currentPlayer === "X" ? "O" : "X"} won the match!
+            </p>
+            <button
+              className=" rounded bg-green-500 text-white font-semibold p-3 mt-5"
+              onClick={resetGame}
+            >
+              Play Again
+            </button>
+          </div>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      )}
+      {ticTacToe.map((item, rowIndex) => {
+        return (
+          <div className="flex justify-center items-center">
+            <div
+              className={`flex justify-center items-center w-72 ${
+                rowIndex === 0 ? "" : "border-t-2 border-t-white"
+              }`}
+              key={rowIndex}
+            >
+              {item.map((el, colIndex) => {
+                return (
+                  <div
+                    className={`w-24 h-24 p-2 flex-shrink-0 flex justify-center items-center cursor-pointer font-semibold text-lg ${
+                      colIndex === 0 ? "" : "border-l-2 border-l-white"
+                    }`}
+                    key={`${rowIndex}${colIndex}`}
+                    onClick={() => handleCellClick(rowIndex, colIndex)}
+                  >
+                    {el}
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        );
+      })}
     </main>
   );
 }
